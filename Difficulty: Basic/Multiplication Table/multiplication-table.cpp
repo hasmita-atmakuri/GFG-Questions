2483c530abfd890aeeb96262c2a95d02c20fd445
hasmita-atmakuri <153909891+hasmita-atmakuri@users.noreply.github.com>
2026-01-18 21:51:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Multiplication Table/multiplication-table.cpp
@@ -0,0 +1,13 @@
+// User function Template for C++
+class Solution {
+  public:
+    vector<int> getTable(int n) {
+        // Write Your Code here
+        vector<int> ans;
+        for(int i=1;i<=10;i++){
+            int an=n*i;
+            ans.push_back(an);
+        }
+        return ans;
+    }
+};
