78dabfdc35ff2f580f78c46995c4fe04bb9b297a
hasmita-atmakuri <153909891+hasmita-atmakuri@users.noreply.github.com>
2026-01-21 00:06:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sum of odd and even elements/sum-of-odd-and-even-elements.cpp
@@ -0,0 +1,20 @@
+
+class Solution {
+  public:
+    vector<int> findSum(int n) {
+        int odd=0;
+        int even=0;
+        
+        for(int i=1;i<=n;i++){
+            if(i%2==0){
+                even+=i;
+            }
+            else
+                odd+=i;
+        }
+        vector<int> ans;
+        ans.push_back(odd);
+        ans.push_back(even);
+        return ans;
+    }
+};
