b6a81dcf8865c2e8ff8a8144ca3844e2605674e1
hasmita-atmakuri <153909891+hasmita-atmakuri@users.noreply.github.com>
2026-01-20 23:22:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sum of elements in a matrix/sum-of-elements-in-a-matrix.cpp
@@ -0,0 +1,14 @@
+// User function template for C++
+
+class Solution {
+  public:
+    int sumOfMatrix(int N, int M, vector<vector<int>> Grid) {
+        int sum=0;
+        for(int i=0;i<N;i++){
+            for(int j=0;j<M;j++){
+                sum+=Grid[i][j];
+            }
+        }
+        return sum;
+    }
+};
