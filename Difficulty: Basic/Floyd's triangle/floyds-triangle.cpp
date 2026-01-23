2779160d5a1d4c24af784ad508a6bc9f402cc49e
hasmita-atmakuri <153909891+hasmita-atmakuri@users.noreply.github.com>
2026-01-23 16:22:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Floyd's triangle/floyds-triangle.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    void printFloydTriangle(int n) {
+        int num=1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                cout<<num<<" ";
+                num++;
+            }
+            cout<<endl;
+        }
+    }
+};
