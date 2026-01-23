a0b48b370bbf97ca8383acb6e85f3e74256adb70
hasmita-atmakuri <153909891+hasmita-atmakuri@users.noreply.github.com>
2026-01-23 16:45:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Diagonal sum/diagonal-sum.cpp
@@ -0,0 +1,14 @@
+// User function Template for C++
+
+class Solution {
+  public:
+    int DiagonalSum(vector<vector<int> >& matrix) {
+        int sum=0;
+        int n=matrix.size();
+        for(int i=0;i<n;i++){
+            sum+=matrix[i][i];
+            sum+=matrix[i][n-i-1];
+        }
+        return sum;
+    }
+};
