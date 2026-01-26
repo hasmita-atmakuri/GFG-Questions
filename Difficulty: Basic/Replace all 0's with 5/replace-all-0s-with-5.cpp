b2990441a7419d08e4b6c4c322ada7cc6caeedc2
hasmita-atmakuri <153909891+hasmita-atmakuri@users.noreply.github.com>
2026-01-26 14:21:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Replace all 0's with 5/replace-all-0s-with-5.cpp
@@ -0,0 +1,13 @@
+/*you are required to complete this method*/
+class Solution {
+  public:
+    int convertFive(int n) {
+        string s=to_string(n);
+        for (char &c : s) {
+            if (c == '0') {
+                c = '5';
+            }
+        }
+        return stoi(s);
+    }
+};
